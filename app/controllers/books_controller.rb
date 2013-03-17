cfdd7646b506d862b312ae689821f1f748365a6f
Wei Lu <wei_lu@acl.com>
2013-03-17 09:00:14 -0700

    a library model

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+	 before_filter :find_book, only: [:show, :edit, :update, :destroy]
+ 
+  def index
+  	@books = Book.all
+  end
+
+  def show
+  	# render :text => "in show"
+  end
+  
+  def new
+  	@book = Book.new
+  end
+
+  def create
+   @book = Book.new(params[:book])
+
+   if @book.save
+     redirect_to @book,notice: "Book created successfully!"
+   else
+    render :new
+   end 	
+  end
+
+  def edit
+  end
+  
+  def update
+   if @book.update_attributes(params[:book])
+     redirect_to @book,notice: "Book updateed successfully!"
+   else
+    render :new
+   end 
+  end
+
+  def destroy
+    if @book.destroy
+    	redirect_to books_path, notice: "Book entry is destroyed"
+    else
+    	redirect_to books_path, notice: "Book entry deletion fails"
+    end
+  end
+
+  private
+
+	def find_book
+	  @book = Book.find(params[:id])
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = '90d2e16aa9c380df0c0cbf54f8353ed1e22f718a635c530b72d87648a1c985fc91dae6ccee5ec4bcf63e4de7391e9d600f4bef774f99e53e26324666bb26a8b1'
